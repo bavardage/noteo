9e87b9f014fe3d54eac27e938b219d83665b8600
Ben Duffield <ben@wintermute.domain.org>
2008-11-21 22:53:06 +0000

    getting there

--- src/Noteo.py
+++ src/Noteo.py
@@ -5,13 +5,14 @@
 
 import time
 import logging
-
+from threading import Timer
 
 class NoteoConfig:
 	def __init__(self, filename, configspec):
 		self.config = ConfigObj(filename, configspec=configspec)
 		self.config.validate(Validator(), copy=True)
 		self.config.write() #write default values
+
 	def __getitem__(self, field):
 		try:
 			return self.config[field]
@@ -23,10 +24,11 @@ class Event(object):
 	def __init__(self, noteo, due_in):
 		self.noteo = noteo
 		self.time = time.time() + due_in
+		self.event_id = id(self)
 	def handle(self):
 		pass
 
-	def handled(self, handler):
+	def handled(self, event, handler=None):
 		pass
 
 	def get_summary(self):
@@ -40,6 +42,24 @@ class Event(object):
 
 	def get_icon(self):
 		pass
+	
+	#comparison
+	def __lt__(self, other):
+		return self.time < other.time
+	def __le__(self, other):
+		return self.time <= other.time
+	def __eq__(self, other):
+		return self.time == other.time
+	def __ne__(self, other):
+		return self.time != other.time
+	def __gt__(self, other):
+		return self.time > other.time
+	def __ge__(self, other):
+		return self.time >= other.time
+	#representations
+	def __repr__(self):
+		return "%s %s" % (self.__class__.__name__, self.event_id)
+
 
 class FunctionCallEvent(Event):
 	def __init__(self, noteo, due_in, function, *args, **kwargs):
@@ -47,6 +67,7 @@ class FunctionCallEvent(Event):
 		self.args = args
 		self.kwargs = kwargs
 		super(FunctionCallEvent, self).__init__(noteo, due_in)
+
 	def handle(self):
 		if callable(self.function):
 			return self.function(*self.args, **self.kwargs)
@@ -55,19 +76,96 @@ class FunctionCallEvent(Event):
 
 
 class NotificationEvent(Event):
-	def __init__(self, due_in, summary, content, icon):
+	def __init__(self, noteo, due_in, summary, content, icon):
 		self.summary = summary
 		self.content = content
 		self.icon = icon
 		super(NotificationEvent, self).__init__(noteo, due_in)
-
-
 	
+	def handle(self):
+		self.noteo.send_to_modules(self)
+
+
+class NoteoModule(object):
+	def __init__(self, noteo):
+		self.noteo = noteo
+
+	def handle_event(self, event):
+		self.noteo.logger.info("Handling event %s" % event)
+		return_val = self.do_handle_event(event)
+		event.handled()
+		return return_val
+
+	def do_handle_event(self, event):
+		pass
+
+
 class Noteo:
 	logger = logging
+	event_loop_running = False
+	event_timer = None
 	def __init__(self):
-		self.logger.basicConfig()
-		self._eventQueue = []
-	def add_event_to_queue(event):
-		self._eventQueue.append(event)
-		self._eventQueue.sort()
+		self.logger.basicConfig(level=logging.DEBUG)
+		self._event_queue = []
+		self._handled_events = {}
+		self._modules = []
+	
+	def add_event_to_queue(self, event):
+		self._event_queue.append(event)
+		#self._event_queue.sort()
+		self.event_loop()
+	
+	def event_handled(self, event=None):
+		self.logger.info("Event(%s) handled" % event)
+		if event in self._handled_events:
+			self._handled_events[event][1] -= 1
+			if self._handled_events[event][1] <= 0:
+				self._handled_events[event][0](event)
+				del self._handled_events[event][0]
+		else:
+			self.logger.error("Event was not in _handled_events")
+		
+	def send_to_modules(self, event):
+		handled = event.handled
+		def new_handled():
+			self.event_handled(event)
+		event.handled = new_handled
+		self._handled_events[event] = [handled, 0]
+		for module in self._modules:
+			self._handled_events[event][1] += 1
+			module.handle_event(event)
+	
+	def handle_event(self, event):
+		if time.time() >= event.time:
+			self.logger.debug("Handling the event %s" % event)
+			event.handle()
+			self._event_queue.remove(event)
+#			self._event_queue.sort()
+		else:
+			self.logger.debug("Event not yet due. Procrastinating")
+	
+	def event_loop(self):
+		if self.event_timer:
+			self.event_timer.cancel()
+			self.event_timer = None
+		if not self.event_loop_running:
+			self.logger.debug("HANDLING EVENTS")
+			self.event_loop_running = True
+			for event in self._event_queue:
+				self.handle_event(event)
+				self._event_queue.sort()
+				if not len(self._event_queue):
+					self.event_loop_running = False
+					return
+				else:
+					self.logger.info("All pending events handled, sleeping")
+					self.logger.info("The event queue now has size %s" %
+							 len(self._event_queue))
+					sleeptime = self._event_queue[0].time - time.time()
+					self.logger.debug("sleeping for %s" % sleeptime)
+					if sleeptime > 0:
+						self.event_loop_running = False
+						if self.event_timer:
+							self.event_timer.cancel()
+						self.event_timer = Timer(sleeptime, self.event_loop)
+						self.event_timer.start()
--- src/test.py
+++ src/test.py
@@ -3,7 +3,23 @@ from Noteo import *
 
 noteo = Noteo()
 
-e = FunctionCallEvent(noteo, lambda x:x*x, 3)
-print e.handle()
-e2 = FunctionCallEvent(noteo, "hi")
-print e2.handle()
+def spam(*args, **kwargs):
+    print "munch munch loverly spam"
+    print args
+    print kwargs
+
+class Mod(NoteoModule):
+    pass
+
+noteo._modules.append(Mod(noteo))
+
+e = FunctionCallEvent(noteo, 1, spam, 3)
+e2 = FunctionCallEvent(noteo, 2, "hi")
+e3 = NotificationEvent(noteo, 3, "this is an event", "content", "no icon")
+def e3_handled(event, handlers=None):
+    print "event(%s) handled by %s" % (event, handlers)
+e3.handled = e3_handled
+noteo.add_event_to_queue(e)
+noteo.add_event_to_queue(e2)
+noteo.add_event_to_queue(e3)
+noteo.event_loop()
