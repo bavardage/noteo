184b112e6b4427d429998c098ac2b85113407a21
DreamTeam <dreamteam69@gmail.com>
2009-02-25 21:26:04 +0000

    Add support for vertical spacing between popups

--- src/modules/Popup.py
+++ src/modules/Popup.py
@@ -9,9 +9,10 @@ class Popup(NoteoModule):
         'verticalArrangement': 'string(default=\'ascending\')',
         'horizontalArrangement': 'string(default=\'right\')',
         'opacity': 'float(default=0.8)',
-        'maxCharsPerLine': 'integer(default=30)',
+	'maxCharsPerLine': 'integer(default=30)',
         'xOffset': 'integer(default=0)',
         'yOffset': 'integer(default=30)',
+	'verticalSpacing': 'integer(default=2)',
         }
     def init(self):
         self.noteo.gtk_required()
@@ -53,21 +54,20 @@ class Popup(NoteoModule):
  
         popup = gtk.Window(gtk.WINDOW_POPUP)
         max_chars = self.config['maxCharsPerLine']
-        
+
         summary_label = gtk.Label()
         summary_label.set_markup(summary)
         summary_label.show()
         summary_label.set_line_wrap(True)
         summary_label.set_width_chars(max_chars)
-        
-        content_label = gtk.Label()
+
+	content_label = gtk.Label()
         content_label.set_markup(content)
         content_label.show()
         content_label.set_line_wrap(True)
         content_label.set_width_chars(max_chars)
 
-        vbox = gtk.VBox()
-        
+	vbox = gtk.VBox()
         vbox.pack_start(summary_label)
         vbox.pack_start(content_label)
 
@@ -75,10 +75,9 @@ class Popup(NoteoModule):
         hbox.pack_start(gtk.image_new_from_pixbuf(icon))
         hbox.pack_start(vbox)
         
-        popup.add(hbox)
+	popup.add(hbox)
         popup.set_opacity(self.config['opacity'])
         popup.show_all()
-        
 
         return popup
 
@@ -95,27 +94,29 @@ class Popup(NoteoModule):
         horizontal_arrangement = self.config['horizontalArrangement']
         xoffset = self.config['xOffset']
         yoffset = self.config['yOffset']
+        vertical_spacing = self.config['verticalSpacing']
         width, height = self._popups[event].get_size()
         self.noteo.logger.debug("Positioning window of size (%s, %s)" % (width, height))
         popup_x, popup_y = xoffset, 0
         if vertical_arrangement == 'descending':
-            greatest_height = yoffset
+            greatest_height = yoffset - intermediateoffset
             for e, p in self._popups.items():
                 w,h = p.get_size()
                 x, y = p.get_position()
                 if (e is not event) and y + h > greatest_height:
                     greatest_height = y + h
-            popup_y = greatest_height
+            popup_y = greatest_height + verticalSpacing
         else:
-            smallest_height = gtk.gdk.screen_height() - yoffset
+            smallest_height = gtk.gdk.screen_height() - yoffset + vertical_spacing
             for e,p in self._popups.items():
                 x, y = p.get_position()
                 if (e is not event) and y < smallest_height:
                     smallest_height = y
-            popup_y = smallest_height - height
+            popup_y = smallest_height - height - vertical_spacing
         if horizontal_arrangement == 'right':
             popup_x = gtk.gdk.screen_width() - width - xoffset
-        self._popups[event].move(popup_x, popup_y)
+        self._popups[event].show_all()
+	self._popups[event].move(popup_x, popup_y)
         self.noteo.gtk_update()
                
             
