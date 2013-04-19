2b29c30ed06bcaab6367773a535d7abb04d1e916
Nuno Cardoso <nunopcardoso@gmail.com>
2013-04-19 21:04:10 +0100

    BatteryCheck: Added better Notifications; trustAcpi option; fibonacciNotify option

--- src/modules/BatteryCheck.py
+++ src/modules/BatteryCheck.py
@@ -4,16 +4,19 @@ import re
 import commands
 
 from Noteo import *
+fibonacci = set([1, 2, 3, 5, 8, 13, 21, 34, 55, 89])
 
 class BatteryCheck(NoteoModule):
     config_spec = {
         'lowPercentage': 'integer(default=10)',
         'criticalPercentage': 'integer(default=5)',
         'pollInterval': 'float(default=60)',
+        'trustAcpi': 'boolean(default=True)',
+        'fibonacciNotify': 'boolean(default=True)',
         }
     notified_low = False
     notified_critical = False
-    state = (100, True)
+    state = (100, None)
     def init(self):
         if self.config['lowPercentage'] < self.config['criticalPercentage']:
             self.noteo.logger.warning(
@@ -38,17 +41,21 @@ class BatteryCheck(NoteoModule):
         menu_item.add_to_queue()
 
     def get_status(self):
-        percentage = 100
-        charging = False
+        percentage, charging = self.state
         time_left = ''
         status = commands.getoutput('acpi')
+
         if self.find_percentage.match(status):
-            percentage = int(self.find_percentage.match(status).groups()[0])
-        if not self.is_discharging.match(status):
-            charging = True
+            new_percentage = int(self.find_percentage.match(status).groups()[0])
+        if self.config['trustAcpi']:
+            charging = not self.is_discharging.match(status)
+        else:
+            if percentage != new_percentage:
+                charging = percentage < new_percentage
+
         if self.find_time_left.match(status):
             time_left = self.find_time_left.match(status).groups()[0]
-        return (percentage, charging, time_left)
+        return (new_percentage, charging, time_left)
 
     def report_current_status(self):
         percentage, charging, time_left = self.get_status()
@@ -57,51 +64,60 @@ class BatteryCheck(NoteoModule):
         if time_left:
             message += "\n%s" % time_left
             message += (' until charged' if charging else ' remaining')
-        notification = NotificationEvent(self.noteo,
-                                         0,
-                                         summary,
-                                         message,
-                                         ('gpm-ac-adapter' if charging else 'battery'),
-                                         )
-        notification.add_to_queue()
+        self.update_notification(NotificationEvent(self.noteo,
+                                                   0,
+                                                   summary,
+                                                   message,
+                                                   ('ac-adapter' if charging else 'battery'),
+                                                  ))
+
+    def update_notification(self, event):
+        event.add_to_queue()
+
 
     def check_battery(self):
+        print("Current state is %s %s" % self.state)
         self.noteo.logger.debug("Current state is %s %s" % self.state)
         percentage, charging, time_left = self.get_status()
+        if self.state[1] is not None and \
+           charging != self.state[1]: #Changed charging state
+            self.update_notification(NotificationEvent(self.noteo,
+                                                       0,
+                                                       "AC Power",
+                                                       "AC power has been plugged %s" % ("in" if charging else "off"),
+                                                       ('ac-adapter' if charging else 'battery'),
+                                                      ))
+
         if charging:
             self.notified_low = False
             self.notified_critical = False
-            if not self.state[1]: #if wasn't previously charging
-                notification = NotificationEvent(self.noteo,
-                                                 0,
-                                                 "AC Power",
-                                                 "AC power has been plugged in",
-                                                 'gpm-ac-adapter'
-                                                 )
-                notification.add_to_queue()
             self.noteo.logger.debug("Previously was %s, now %s" %
                                     (self.state[1], charging))
         else:
             if percentage < self.config['criticalPercentage'] \
                     and not self.notified_critical:
                 self.notified_critical = True
-                notification = NotificationEvent(self.noteo,
+                self.update_notification(NotificationEvent(self.noteo,
                                                  0,
                                                  "Battery Critical",
                                                  "Battery charge is only %s%%" % percentage,
                                                  'battery'
-                                                 )
-                notification.add_to_queue()
+                                                 ))
             elif percentage < self.config['lowPercentage'] \
                     and not self.notified_low:
                 self.notified_low = True
-                notification = NotificationEvent(self.noteo,
+                self.update_notification(NotificationEvent(self.noteo,
                                                  0,
                                                  "Battery Low",
                                                  "Battery charge is at %s%%" % percentage,
                                                  'battery'
-                                                 )
-                notification.add_to_queue()
+                                                 ))
+
+        if percentage != self.state[0] and \
+           self.config['fibonacciNotify'] and \
+           percentage in fibonacci:
+            self.report_current_status()
+
         self.noteo.logger.debug("Setting status to percentage: %s, charging %s" %
                          (percentage, charging))
         self.state = (percentage, charging)
