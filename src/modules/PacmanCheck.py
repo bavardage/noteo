2c120a65b8cfe8ea7e19c67863d7bea9c98f2f02
Nuno Cardoso <nunopcardoso@gmail.com>
2013-04-19 21:32:38 +0100

    PacmanCheck: Fixed and improved module

--- src/modules/PacmanCheck.py
+++ src/modules/PacmanCheck.py
@@ -4,12 +4,13 @@ import re
 from Noteo import *
 
 class PacmanCheck(NoteoModule):
-    no_updates = re.compile('.+no upgrades found.+', re.DOTALL)
-    number_of_updates = re.compile('.+Targets \((\d+)\).+', re.DOTALL)
     config_spec = {
         'pollInterval': 'float(default=300)',
+        'iterationsBeforeReminder': 'integer(default=10)',
         }
     def init(self):
+        self._last_count = 0
+        self._reminder = 0
         self.check_event = RecurringFunctionCallEvent(self.noteo,
                                                       self.check,
                                                       self.config['pollInterval']
@@ -17,25 +18,27 @@ class PacmanCheck(NoteoModule):
         self.check_event.add_to_queue()
 
     def check(self):
-        status = commands.getoutput('pacman -Qu')
-        if not re.match(self.no_updates, status):
-            updates = re.match(self.number_of_updates,
-                               status,
-                               ).groups()[0]
-            summary = 'System Updates'
-            plural  = (int(updates) > 1)
-            message = '%s package%s need%s updating' % (
-                updates,
-                ('s' if plural else ''),
-                ('' if plural else 's')
-                )
-            notification = NotificationEvent(self.noteo,
-                                             0,
-                                             summary,
-                                             message,
-                                             'system',
-                                             )
-            notification.add_to_queue()
+        status = commands.getoutput('pacman -Qu').split('\n')
+        if len(status):
+           if len(status) != self._last_count or \
+              (self._reminder == 0 and self.config['iterationsBeforeReminder'] != 0):
+               summary = 'System Updates'
+               plural  = (len(status) > 1)
+               message = '%s package%s need%s updating' % (
+                   len(status),
+                   ('s' if plural else ''),
+                   ('' if plural else 's')
+               )
+               notification = NotificationEvent(self.noteo,
+                                                0,
+                                                summary,
+                                                message,
+                                                'system',
+                                               )
+               notification.add_to_queue()
+               self._reminder = self.config['iterationsBeforeReminder'] + 1
+        self._reminder -= 1
+        self._last_count = len(status)
         return True
 
 module = PacmanCheck
