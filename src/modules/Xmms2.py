dfd9ff78ba63975d75405b662d43eb76b8dedb77
Nuno Cardoso <nunopcardoso@gmail.com>
2013-04-21 11:23:26 +0100

    Fixed Xmms2

--- src/modules/Xmms2.py
+++ src/modules/Xmms2.py
@@ -3,34 +3,24 @@ import commands
 from Noteo import *
 
 class Xmms2(NoteoModule):
-    config_spec = {
-        'pollInterval': 'float(default=2.0)',
-        }
+    config_spec = {'pollInterval': 'float(default=2.0)'}
     def init(self):
         self.current_song = self.get_current_song()
-        self.update_event = RecurringFunctionCallEvent(self.noteo,
-                                                       self.update,
-                                                       self.config['pollInterval']
-                                                       )
-        self.update_event.add_to_queue()
-        notify_current_song_menu_item = CreateMenuItemEvent(self.noteo,
-                                                            "Show current song",
-                                                            self.notify_current_song,
-                                                            icon='audio-x-generic'
-                                                            )
-        notify_current_song_menu_item.add_to_queue()
+        update_event = FunctionCallEvent(self.update)
+        update_event.recurring_delay = self.config['pollInterval']
+        self.noteo.add_event(update_event)
+
+        self.noteo.add_event(CreateMenuItemEvent("Show current song",
+                                                 self.notify_current_song,
+                                                 icon='audio-x-generic'))
 
     def get_current_song(self):
         return commands.getoutput("xmms2 current")
 
     def notify_current_song(self):
-        notification = NotificationEvent(self.noteo,
-                                             0,
-                                             "Now playing",
-                                             self.current_song,
-                                             'audio-x-generic'
-                                             )
-        notification.add_to_queue()
+        self.noteo.add_event(NotificationEvent("Now playing",
+                                               self.current_song,
+                                               'audio-x-generic'))
 
     def update(self):
         self.last_song = self.current_song
